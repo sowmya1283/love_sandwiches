e0bcc697054a28809ab2b0318f84e2f11c87f434
sowmya1283 <75764956+sowmya1283@users.noreply.github.com>
2025-02-11 11:49:25 +0000

    added validation to provide exactly 6 values of data

--- run.py
+++ run.py
@@ -28,11 +28,24 @@ def get_sales_data():
     print("Data should be six numbers, separated by commas.")
     print("Example: 10,20,30,40,50,60\n")
 
-data_str =  input("Enter your data here: ")
+    data_str =  input("Enter your data here: ")
 
-print(f"the data provided is {data_str}")
+    print(f"the data provided is {data_str}")
 
-   # data_str = input("Enter your data here: ")
-   # print(f"The data provided is {data_str}")
+    sales_data= list(data_str.split(","))
+#sales_data = data_str.split(",")
+   # print(sales_data)
+    validate_data(sales_data)
+
+def validate_data(values):
+
+  # print(values)
+    try:
+        if len(values)!=6:
+            raise ValueError(
+        f"Exactly 6 values required, you provided {len(values)}"
+    )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again. \n")
 
 get_sales_data()
