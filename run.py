77c3b9645ebad293d5caefa2820e49f569c49abb
sowmya1283 <75764956+sowmya1283@users.noreply.github.com>
2025-02-11 13:37:43 +0000

    added data validation for integer values only

--- run.py
+++ run.py
@@ -39,8 +39,9 @@ def get_sales_data():
 
 def validate_data(values):
 
-  # print(values)
+    print(values)
     try:
+        [int(value) for value in values]
         if len(values)!=6:
             raise ValueError(
         f"Exactly 6 values required, you provided {len(values)}"
